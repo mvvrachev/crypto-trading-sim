400513fe09b11c73398703518a5fbe3ca7d30ab5
mvvrachev <martin.v.vrachev.2017@elsys-bg.org>
2025-06-12 19:20:39 +0300

    Create Spring project

---
+++ backend/crypto-trading-sim-server/src/main/java/com/trading212/cryptotradingsimserver/CryptoTradingSimServerApplication.java
@@ -0,0 +1,13 @@
+package com.trading212.cryptotradingsimserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingSimServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoTradingSimServerApplication.class, args);
+    }
+
+}
